506c900c55f0489e8a2e29d7cd8cd910692629b1
Struan Donald <struan@exo.org.uk>
2022-10-05 14:44:59 +0100

    initial basic area model

--- hub/models.py
+++ hub/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Area(models.Model):
+    mapit_id = models.CharField(max_length=30)
+    gss = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
+    area_type = models.CharField(max_length=20)
