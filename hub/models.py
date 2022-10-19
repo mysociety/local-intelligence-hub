af94af16e1a9363e89904cba80372615cd2a161a
Struan Donald <struan@exo.org.uk>
2022-10-19 14:16:58 +0100

    update area model to make GSS unique
    Should only ever be one area per GSS code

--- hub/models.py
+++ hub/models.py
@@ -11,7 +11,7 @@ class DataType(models.Model):
 
 class Area(models.Model):
     mapit_id = models.CharField(max_length=30)
-    gss = models.CharField(max_length=30)
+    gss = models.CharField(unique=True, max_length=30)
     name = models.CharField(max_length=200)
     area_type = models.CharField(max_length=20)
     geometry = models.TextField(blank=True, null=True)
