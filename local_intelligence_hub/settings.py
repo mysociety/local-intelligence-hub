6a7b334847ca481601dfb65467a7dc671244c325
Alex Parsons <alex.parsons@mysociety.org>
2022-11-04 14:38:55 +0000

    Allow form submissions in codespaces

--- local_intelligence_hub/settings.py
+++ local_intelligence_hub/settings.py
@@ -186,6 +186,7 @@ BOOTSTRAP5 = {
 if DEBUG and HIDE_DEBUG_TOOLBAR is False:  # pragma: no cover
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    CSRF_TRUSTED_ORIGINS = ["https://*.preview.app.github.dev"]
 
     # debug toolbar has to come after django_hosts middleware
     MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
