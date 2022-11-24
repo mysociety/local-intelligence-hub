edc8893f11a8f94b80f5f6fd8df37e6d01b3a985
Struan Donald <struan@exo.org.uk>
2022-11-24 15:42:07 +0000

    move a chunk of common code out the base importers

--- hub/management/commands/base_importers.py
+++ hub/management/commands/base_importers.py
@@ -5,8 +5,9 @@ from django.db.models import Avg, IntegerField
 from django.db.models.functions import Cast
 
 import pandas as pd
+from tqdm import tqdm
 
-from hub.models import Area, AreaData
+from hub.models import Area, AreaData, DataSet, DataType
 from utils.mapit import (
     BadRequestException,
     ForbiddenException,
@@ -17,7 +18,106 @@ from utils.mapit import (
 )
 
 
-class BaseLatLongImportCommand(BaseCommand):
+class BaseAreaImportCommand(BaseCommand):
+    data_types = {}
+    cast_field = IntegerField
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-q", "--quiet", action="store_true", help="Silence progress bars."
+        )
+
+    def get_label(self, config):
+        return config["defaults"]["label"]
+
+    def delete_data(self):
+        pass
+
+    def add_data_sets(self, df=None):
+        for name, config in self.data_sets.items():
+            label = self.get_label(config)
+
+            data_set, created = DataSet.objects.update_or_create(
+                name=name,
+                defaults={
+                    "label": label,
+                    "description": label,
+                    **config["defaults"],
+                },
+            )
+
+            data_type, created = DataType.objects.update_or_create(
+                data_set=data_set,
+                name=name,
+                defaults={
+                    "data_type": config["defaults"]["data_type"],
+                    "label": label,
+                    "description": label,
+                },
+            )
+
+            self.data_types[name] = data_type
+
+    def update_averages(self):
+        for data_type in self.data_types.values():
+            average = (
+                AreaData.objects.filter(data_type=data_type)
+                .annotate(cast_data=Cast("data", output_field=self.cast_field()))
+                .all()
+                .aggregate(Avg("cast_data"))
+            )
+
+            data_type.average = average["cast_data__avg"]
+            data_type.save()
+
+
+class BaseImportFromDataFrameCommand(BaseAreaImportCommand):
+    uses_gss = True
+
+    def process_data(self, df):
+        if not self._quiet:
+            self.stdout.write(self.message)
+
+        for index, row in tqdm(df.iterrows(), disable=self._quiet, total=df.shape[0]):
+            cons = row[self.cons_row]
+
+            if not pd.isna(cons):
+                if self.uses_gss:
+                    areas = Area.objects.filter(gss=cons)
+                else:
+                    areas = Area.objects.filter(name=cons)
+
+                areas = list(areas)
+
+            if len(areas) == 0:
+                if self.uses_gss:
+                    self.stdout.write(f"Failed to find area with code {cons}")
+                else:
+                    self.stdout.write(f"no matching area for {cons}")
+                continue
+
+            for area in areas:
+                try:
+                    for name, conf in self.data_sets.items():
+                        area_data, created = AreaData.objects.get_or_create(
+                            data_type=self.data_types[name],
+                            area=area,
+                            defaults={"data": row[conf["col"]]},
+                        )
+                except Exception as e:
+                    print(f"issue with {cons}: {e}")
+                    break
+
+    def handle(self, quiet=False, *args, **options):
+        self._quiet = quiet
+        df = self.get_dataframe()
+        self.add_data_sets(df)
+        self.delete_data()
+        self.process_data(df)
+        self.update_averages()
+
+
+class BaseLatLongImportCommand(BaseAreaImportCommand):
     def _process_lat_long(self, lat=None, lon=None, row_name=None):
         if not pd.isna(lat) and not pd.isna(lon):
             try:
@@ -60,14 +160,3 @@ class BaseLatLongImportCommand(BaseCommand):
         # retry once if it fails so we can catch rate limit errors
         if success is False:
             self._process_lat_long(lat=lat, lon=lon, row_name=row_name)
-
-    def update_average(self):
-        average = (
-            AreaData.objects.filter(data_type=self.data_type)
-            .annotate(data_as_int=Cast("data", output_field=IntegerField()))
-            .all()
-            .aggregate(Avg("data_as_int"))
-        )
-
-        self.data_type.average = average["data_as_int__avg"]
-        self.data_type.save()
