a84d89cd5264555745870ac46140785dc25131fd
Zarino Zappia <mail@zarino.co.uk>
2022-11-24 14:49:01 +0000

    Append site title to end of page title

--- hub/mixins.py
+++ hub/mixins.py
@@ -1,6 +1,11 @@
 class TitleMixin:
+    site_title = "Local Intelligence Hub"
+
     def get_page_title(self):
-        return self.page_title
+        if self.page_title:
+            return f"{self.page_title} | {self.site_title}"
+        else:
+            return f"{self.site_title}"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
