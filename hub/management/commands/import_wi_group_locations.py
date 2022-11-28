39f9dcd3c611cf9632f2378d626b49e0756921fd
Struan Donald <struan@exo.org.uk>
2022-11-28 14:16:41 +0000

    fix WI import script average calculation
    also bring it in line with other import scripts

--- hub/management/commands/import_wi_group_locations.py
+++ hub/management/commands/import_wi_group_locations.py
@@ -4,7 +4,7 @@ from time import sleep
 import requests
 from tqdm import tqdm
 
-from hub.models import AreaData, DataSet, DataType
+from hub.models import AreaData
 
 from .base_importers import BaseLatLongImportCommand
 
@@ -12,32 +12,22 @@ from .base_importers import BaseLatLongImportCommand
 class Command(BaseLatLongImportCommand):
     help = "Import data about number WI groups of per constituency"
 
-    def add_data_sets(self):
-        data_set, created = DataSet.objects.update_or_create(
-            name="constituency_wi_group_count",
-            defaults={
-                "label": "Number of Women’s Institute groups",
-                "description": "Number of Women’s Institute groups",
-                "data_type": "integer",
-                "category": "movement",
-                "source_label": "Woman’s Institute",
-                "source": "https://www.thewi.org.uk/",
-                "source_type": "api",
-                "data_url": "https://wi-search.squiz.cloud/s/search.json?collection=nfwi-federations&profile=_default&query=!null&sort=prox&sort=prox&start_rank=1&origin=54.093409,-2.89479&maxdist=9999&num_ranks=9999",
-            },
-        )
+    defaults = {
+        "label": "Number of Women’s Institute groups",
+        "description": "Number of Women’s Institute groups",
+        "data_type": "integer",
+        "category": "movement",
+        "source_label": "Woman’s Institute",
+        "source": "https://www.thewi.org.uk/",
+        "source_type": "api",
+        "data_url": "https://wi-search.squiz.cloud/s/search.json?collection=nfwi-federations&profile=_default&query=!null&sort=prox&sort=prox&start_rank=1&origin=54.093409,-2.89479&maxdist=9999&num_ranks=9999",
+    }
 
-        data_type, created = DataType.objects.update_or_create(
-            data_set=data_set,
-            name="constituency_wi_group_count",
-            defaults={
-                "data_type": "integer",
-                "label": "Number of Women’s Institute groups",
-                "description": "Number of Women’s Institute groups",
-            },
-        )
-
-        self.data_type = data_type
+    data_sets = {
+        "constituency_wi_group_count": {
+            "defaults": defaults,
+        },
+    }
 
     def get_api_results(self):
         results = requests.get(self.data_type.data_set.data_url)
@@ -46,8 +36,12 @@ class Command(BaseLatLongImportCommand):
 
         return data
 
+    def delete_data(self):
+        for data_type in self.data_types.values():
+            AreaData.objects.filter(data_type=data_type).delete()
+
     def process_data(self):
-        AreaData.objects.filter(data_type=self.data_type).delete()
+        self.data_type = self.data_types["constituency_wi_group_count"]
 
         if not self._quiet:
             self.stdout.write("Importing women's institute group data")
@@ -74,6 +68,7 @@ class Command(BaseLatLongImportCommand):
 
     def handle(self, quiet=False, *args, **options):
         self._quiet = quiet
+        self.delete_data()
         self.add_data_sets()
         self.process_data()
-        self.update_average()
+        self.update_averages()
