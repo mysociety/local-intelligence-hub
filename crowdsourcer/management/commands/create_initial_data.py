4066e8b54f74abe05c3df90e6557f3ef5900deb7
Struan Donald <struan@exo.org.uk>
2022-12-19 15:08:35 +0000

    use correct groups, council types and match appropriately
    Also import sections

--- crowdsourcer/management/commands/create_initial_data.py
+++ crowdsourcer/management/commands/create_initial_data.py
@@ -1,18 +1,22 @@
-import json
-
-from crowdsourcer.models import PublicAuthority, QuestionGroup
 from django.core.management.base import BaseCommand
+
+from crowdsourcer.models import PublicAuthority, QuestionGroup, Section
 from utils import mapit
 
 
 class Command(BaseCommand):
     help = "set up authorities and question groups"
 
-    groups = [
-        "Scottish Councils",
-        "Single Tier",
-        "Welsh",
-        "Other",
+    groups = ["Single Tier", "District", "County", "Northern Ireland"]
+
+    sections = [
+        "Buildings & Heating",
+        "Transport",
+        "Planning & Land Use",
+        "Governance & Finance",
+        "Biodiversity",
+        "Collaboration & Engagement",
+        "Waste Reduction & Food",
     ]
 
     def add_arguments(self, parser):
@@ -21,30 +25,42 @@ class Command(BaseCommand):
         )
 
     def get_group(self, props):
-        group = "Other"
+        group = "District"
 
-        if props["country"] == "S":
-            group = "Scottish Councils"
+        print(props["name"], props["type"])
+        if props["type"] == "LGD":
+            group = "Northern Ireland"
         elif props["country"] == "W":
-            group = "Welsh"
-        elif props["type"] in ["LBO", "UA"]:
             group = "Single Tier"
+        elif props["country"] == "S":
+            group = "Single Tier"
+        elif props["type"] in ["CC", "MTD", "LBO", "UA"]:
+            group = "Single Tier"
+        elif props["type"] in ["CTY"]:
+            group = "County"
 
         g = QuestionGroup.objects.get(description=group)
         return g
 
     def handle(self, quiet: bool = False, *args, **options):
+        for section in self.sections:
+            c, c = Section.objects.get_or_create(title=section)
+
         for group in self.groups:
             g, c = QuestionGroup.objects.get_or_create(description=group)
 
         mapit_client = mapit.MapIt()
-        areas = mapit_client.areas_of_type(["CTY", "LBO", "NMD", "MD", "UA", "NID"])
+        areas = mapit_client.areas_of_type(
+            ["CTY", "LBO", "NMD", "UTA", "LGD", "CC", "DIS", "MTD"]
+        )
 
         if not quiet:
             print("Importing Areas")
         for area in areas:
-            a, created = PublicAuthority.objects.get_or_create(
+            a, created = PublicAuthority.objects.update_or_create(
                 unique_id=area["codes"]["gss"],
-                name=area["name"],
-                questiongroup=self.get_group(area),
+                defaults={
+                    "name": area["name"],
+                    "questiongroup": self.get_group(area),
+                },
             )
