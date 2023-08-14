700f2609215a051d1596cca1d9f8e6621321a564
Struan Donald <struan@exo.org.uk>
2023-08-14 10:56:34 +0100

    fixup! expose dataset release date in more info bit of filter

--- hub/migrations/0049_add_dataset_release_date.py
+++ hub/migrations/0049_add_dataset_release_date.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("hub", "0041_alter_dataset_subcategory"),
+        ("hub", "0048_alter_area_area_type"),
     ]
 
     operations = [
