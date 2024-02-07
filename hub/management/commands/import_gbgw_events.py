12f6eaf0c36dd0387505bf075d5d2b340a29ac7e
Struan Donald <struan@exo.org.uk>
2024-02-07 17:47:37 +0000

    update GBGW to get data from processed file

--- hub/management/commands/import_gbgw_events.py
+++ hub/management/commands/import_gbgw_events.py
@@ -1,32 +1,21 @@
-from time import sleep
-
 from django.conf import settings
 
-import pandas as pd
-from tqdm import tqdm
+from hub.models import DataSet
 
-from hub.models import AreaData, DataSet, DataType
-
-from .base_importers import BaseLatLongImportCommand
+from .base_importers import BaseConstituencyCountImportCommand
 
 
-class Command(BaseLatLongImportCommand):
+class Command(BaseConstituencyCountImportCommand):
     help = "Import data about number of GBGW events per constituency"
+    message = "Importing 2022 GBGW events"
+    uses_gss = False
 
-    data_file = settings.BASE_DIR / "data" / "gbgw_events.csv"
-    column_map = {}
-    data_types = {}
+    data_file = settings.BASE_DIR / "data" / "gbgw_events_processed.csv"
+    cons_col = "area"
 
-    def get_dataframe(self):
-        df = pd.read_csv(self.data_file)
-        df = df.dropna(axis="columns", how="all")
-
-        return df
-
-    def add_data_sets(self, df):
-        data_set, created = DataSet.objects.update_or_create(
-            name="constituency_gbgw_2022_event_count",
-            defaults={
+    data_sets = {
+        "constituency_gbgw_2022_event_count": {
+            "defaults": {
                 "label": "Number of Great Big Green Week 2022 events",
                 "release_date": "October 2022",
                 "data_type": "integer",
@@ -41,45 +30,6 @@ class Command(BaseLatLongImportCommand):
                 "comparators": DataSet.numerical_comparators(),
                 "unit_type": "raw",
                 "unit_distribution": "people_in_area",
-            },
-        )
-
-        data_type, created = DataType.objects.update_or_create(
-            data_set=data_set,
-            name="constituency_gbgw_2022_event_count",
-            area_type=self.get_area_type(),
-            defaults={
-                "data_type": "integer",
-                "label": "Number of Great Big Green Week 2022 events",
-            },
-        )
-
-        self.data_type = data_type
-        self.data_types[data_type.name] = data_type
-
-    def process_data(self, df):
-        AreaData.objects.filter(data_type=self.data_type).delete()
-
-        if not self._quiet:
-            self.stdout.write("Importing great big green week data")
-        for index, row in tqdm(df.iterrows(), disable=self._quiet, total=df.shape[0]):
-            lat = row["Latitude"]
-            lon = row["Longitude"]
-
-            self.process_lat_long(lat=lat, lon=lon, row_name=index)
-
-            if index > 0 and index % 50 == 0:
-                sleep(10)
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "-q", "--quiet", action="store_true", help="Silence progress bars."
-        )
-
-    def handle(self, quiet=False, *args, **options):
-        self._quiet = quiet
-        df = self.get_dataframe()
-        self.add_data_sets(df)
-        self.process_data(df)
-        self.update_averages()
-        self.update_max_min()
+            }
+        }
+    }
