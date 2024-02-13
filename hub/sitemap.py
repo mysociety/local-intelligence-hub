891f794ac5639da623995787a71b317ff0cd0d8f
Struan Donald <struan@exo.org.uk>
2024-02-13 14:15:59 +0000

    add a sitemap
    Fixes #371

---
+++ hub/sitemap.py
@@ -0,0 +1,31 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from hub.models import Area
+
+
+class StaticViewSitemap(Sitemap):
+    def items(self):
+        return [
+            "about",
+            "contact",
+            "explore",
+            "future-constituencies",
+            "privacy",
+            "sources",
+            "terms",
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class AreaSiteMap(Sitemap):
+    def items(self):
+        return Area.objects.all()
+
+
+hub_sitemap = {
+    "static": StaticViewSitemap,
+    "area": AreaSiteMap,
+}
