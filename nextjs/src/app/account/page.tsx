01ad4e78a1c14276abea156e6521e5c29ed4bad6
Moggach <acunnane13@gmail.com>
2024-03-04 12:19:17 +0000

    add account page for login to redirect to

--- nextjs/src/app/account/page.tsx
+++ nextjs/src/app/account/page.tsx
@@ -1,10 +1,13 @@
+'use client';
+
+import { useRequireAuth } from '../../components/authenticationHandler';
 
 export default function Account() {
+  useRequireAuth();
 
-    return (
-        <>
-            <h1>Welcome to your Mapped account</h1>
-     
-        </>
-    );
+  return (
+    <>
+      <h1>Welcome to your Mapped Account</h1>
+    </>
+  );
 }
