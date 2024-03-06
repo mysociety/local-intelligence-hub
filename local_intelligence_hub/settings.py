55538b66cee7d89fab3b3f5b0492a48581078adb
Moggach <acunnane13@gmail.com>
2024-03-06 10:49:09 +0000

    remove manifeststaticfilestoragesetting

--- local_intelligence_hub/settings.py
+++ local_intelligence_hub/settings.py
@@ -194,11 +194,6 @@ STATICFILES_DIRS = [
     ("vue", BASE_DIR / "vendor" / "vue" / "js"),
 ]
 
-# only want to do this for live really
-if not DEBUG:  # pragma: no cover
-    STATICFILES_STORAGE = (
-        "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-    )
 
 COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 COMPRESS_CSS_HASHING_METHOD = "content"
