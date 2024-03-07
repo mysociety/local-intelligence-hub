2e920f148f22febbbc22f696dd3db554f2895c58
Moggach <acunnane13@gmail.com>
2024-03-07 17:58:14 +0000

    customise border color and add top border to footer

--- nextjs/src/components/footer.tsx
+++ nextjs/src/components/footer.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="p-lg flex flex-row justify-between items-center">
-            <div className="flex flex-row items-center gap-[100px]">
+        <footer className="mx-lg flex flex-row justify-between items-center border-t-[1px]">
+            <div className="flex flex-row items-center gap-[100px] pt-md">
                 <Link href="https://commonknowledge.coop/"><CommonKnowledgeLogo /></Link>
                 <Link href="https://www.civicpower.org.uk/">   <CiviPowerFundLogo /></Link></div>
             <Link href="/" className="text-muted font-IBMPlexMono text-[15px] tracking-[-0.3px] leading-[150%]">Privacy policy</Link>
