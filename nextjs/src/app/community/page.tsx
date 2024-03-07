f63cf2dfc2d41eae291eacfb46d2510fbc1aade1
Moggach <acunnane13@gmail.com>
2024-03-07 18:13:38 +0000

    add additional pages skeletons

---
+++ nextjs/src/app/community/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Community() {
+
+  return (
+    <>
+      <h1 className="text-hLg">Community</h1>
+
+    </>
+  );
+}
---
+++ nextjs/src/app/support/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Support() {
+
+    return (
+        <>
+            <h1 className="text-hLg">Support</h1>
+        </>
+    );
+}
