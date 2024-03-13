8d4e7b5ab033981c7ef646d0c40a4bbdd84f4cec
Struan Donald <struan@exo.org.uk>
2024-03-13 14:38:37 +0000

    imports for council climate emergency declarations and net zero targets

---
+++ hub/management/commands/import_council_carbon_neutral_data.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from mysoc_dataset import get_dataset_url
+
+from hub.models import Area, AreaData, DataSet
+
+from .base_importers import BaseImportFromDataFrameCommand, MultipleAreaTypesMixin
+
+declare_map = {
+    "Y": "Yes",
+    "N": "No",
+}
+
+
+class Command(MultipleAreaTypesMixin, BaseImportFromDataFrameCommand):
+    cons_row = "gss_code"
+    message = "Importing council net zero target data"
+    uses_gss = True
+    do_not_convert = True
+
+    area_types = ["STC", "DIS"]
+
+    defaults = {
+        "label": "Council Net Zero target date",
+        "data_type": "integer",
+        "category": "place",
+        "source_label": "Data from mySociety",
+        "source": "https://pages.mysociety.org/la-plans-promises/",
+        "source_type": "csv",
+        "table": "areadata",
+        "data_url": "https://pages.mysociety.org/la-plans-promises/downloads/local-authority-climate-emergency-declarations-declarations-csv/latest",
+        "comparators": DataSet.numerical_comparators(),
+        "is_filterable": True,
+        "is_shadable": True,
+        "is_public": True,
+        "unit_type": "raw",
+        "unit_distribution": "physical_area",
+    }
+
+    data_sets = {
+        "council_net_zero_date": {
+            "defaults": defaults,
+            "col": "year",
+        },
+    }
+
+    def get_dataframe(self):
+        url = get_dataset_url(
+            repo_name="la-plans-promises",
+            package_name="local_authority_net_zero_commitments",
+            version_name="latest",
+            file_name="promises.csv",
+            done_survey=True,
+        )
+        df = pd.read_csv(url)
+
+        councils = []
+        for index, row in df.iterrows():
+            if pd.isna(row["target"]):
+                continue
+            councils.append(
+                {
+                    "gss_code": row["gss_code"],
+                    "year": row["target"],
+                }
+            )
+
+        df = pd.DataFrame(councils)
+
+        return df
