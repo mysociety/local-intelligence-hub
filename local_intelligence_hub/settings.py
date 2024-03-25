47f5cac2f47be631c1837ab2bddfe7595f18ef9c
Moggach <acunnane13@gmail.com>
2024-03-25 19:20:20 +0000

    try setting environments for sentry

--- local_intelligence_hub/settings.py
+++ local_intelligence_hub/settings.py
@@ -290,6 +290,7 @@ SCHEDULED_UPDATE_SECONDS_DELAY = env("SCHEDULED_UPDATE_SECONDS_DELAY")
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
+    environment=os.getenv("SENTRY_ENV"),
     integrations=[DjangoIntegration()],
     # Optionally, you can adjust the logging level
     traces_sample_rate=1.0,  # Adjust sample rate as needed
