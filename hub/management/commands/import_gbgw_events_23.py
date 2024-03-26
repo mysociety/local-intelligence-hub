45ca36067e844cced0f374b6aed3024e6c90b475
Struan Donald <struan@exo.org.uk>
2024-03-26 16:27:53 +0000

    update GBGW importers to handle councils

--- hub/management/commands/import_gbgw_events_23.py
+++ hub/management/commands/import_gbgw_events_23.py
@@ -2,16 +2,23 @@ from django.conf import settings
 
 from hub.models import DataSet
 
-from .base_importers import BaseConstituencyCountImportCommand
+from .base_importers import BaseConstituencyCountImportCommand, MultipleAreaTypesMixin
 
 
-class Command(BaseConstituencyCountImportCommand):
+class Command(MultipleAreaTypesMixin, BaseConstituencyCountImportCommand):
     help = "Import data about number of GBGW events in 2023 per constituency"
     message = "Importing 2023 GBGW events"
     uses_gss = False
 
     data_file = settings.BASE_DIR / "data" / "gbgw_events_23_processed.csv"
-    cons_col = "area"
+
+    area_types = ["WMC", "WMC23", "STC", "DIS"]
+    cons_col_map = {
+        "WMC": "WMC",
+        "WMC23": "WMC23",
+        "STC": "STC",
+        "DIS": "DIS",
+    }
 
     data_sets = {
         "constituency_gbgw_2023_event_count": {
