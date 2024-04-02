87b2bcef2848d80bf7a4db14baf9ab04d1be25d7
Moggach <acunnane13@gmail.com>
2024-04-02 14:12:05 +0000

    add logo to footer

--- nextjs/src/components/footer.tsx
+++ nextjs/src/components/footer.tsx
@@ -8,7 +8,8 @@ export default function Footer() {
         <footer className="mx-16 py-8 flex flex-row justify-between items-center border-t border-meepGray-700">
             <div className="flex flex-row items-center gap-[100px] pt-md">
                 <Link href="https://commonknowledge.coop/"><CommonKnowledgeLogo /></Link>
-                <Link href="https://www.civicpower.org.uk/">   <CiviPowerFundLogo /></Link></div>
+                <Link href="https://www.civicpower.org.uk/">   <CiviPowerFundLogo /></Link>
+                <Link href="https://www.jrf.org.uk/"> <JRRTLogo /></Link></div>
             <Link href="/" className="text-muted font-IBMPlexMono text-[15px] tracking-[-0.3px] leading-[150%]">Privacy policy</Link>
         </footer>)
 }
