87ee03beb9bdefb99f60a5d4c2895547f43a1187
Moggach <acunnane13@gmail.com>
2024-04-03 17:31:12 +0000

    add waiting list sign up form that posts to mailchimp

--- nextjs/src/components/WaitListSignUp.tsx
+++ nextjs/src/components/WaitListSignUp.tsx
@@ -10,50 +10,41 @@ const MailchimpForm = () => {
     const [role, setRole] = useState('');
     const [heardAboutMapped, setHeardAboutMapped] = useState('');
     const [whyMappedUseful, setWhyMappedUseful] = useState('');
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState('');
 
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-        let url = 'https://commonknowledge.us19.list-manage.com/subscribe/post?u=7d61a70102ab811e6282bee60&amp;id=b87cfcec60&amp;f_id=00788fe4f0';
-        url = `${url}&EMAIL=${email}&FNAME=${firstName}&MMERGE3=${organisationName}&MMERGE4=${role}&MMERGE5=${heardAboutMapped}&MMERGE6=${whyMappedUseful}`
-        fetch(url, {
-            method: "post",
-            mode: 'no-cors'
-        }).then((r) => {
-            console.log('response', r)
-            setMessage('Thank you for signing up!');
-        })
-            .catch((e) => {
-                console.log('error', e)
-            })
-    };
+    const formActionURL = 'https://commonknowledge.us19.list-manage.com/subscribe/post?u=7d61a70102ab811e6282bee60&amp;id=b87cfcec60&amp;f_id=00788fe4f0';
 
     return (
         <>
-            {!message &&
-                <form className="pb-4 flex flex-col space-y-4" onSubmit={handleSubmit}>
+            {message ? (
+                <div>{message}</div>
+            ) : (
+                <form className="pb-4 flex flex-col space-y-4" action={formActionURL} method="POST" target="_blank">
                     <label htmlFor="firstName">First Name</label>
-                    <Input id="firstName" required={true} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    <Input id="firstName" name="FNAME" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                     <label htmlFor="secondName">Second Name</label>
-                    <Input id="secondName" value={secondName} onChange={(e) => setSecondName(e.target.value)} />
+                    <Input id="secondName" name="LNAME" value={secondName} onChange={(e) => setSecondName(e.target.value)} />
                     <label htmlFor="email">Email</label>
-                    <Input id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <Input id="email" name="EMAIL" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <label htmlFor="organisationName">Main organisation name</label>
-                    <Input id="organisationName" value={organisationName} onChange={(e) => setOrganisationName(e.target.value)} />
-                    <label htmlFor="role">What{"'"}s your role?</label>
-                    <Input id="role" value={role} onChange={(e) => setRole(e.target.value)} />
+                    <Input id="organisationName" name="ORG" value={organisationName} onChange={(e) => setOrganisationName(e.target.value)} />
+                    <label htmlFor="role">What's your role?</label>
+                    <Input id="role" name="ROLE" value={role} onChange={(e) => setRole(e.target.value)} />
                     <label htmlFor="heardAboutMapped">How did you hear about Mapped?</label>
-                    <Input id="heardAboutMapped" value={heardAboutMapped} onChange={(e) => setHeardAboutMapped(e.target.value)} />
+                    <Input id="heardAboutMapped" name="MAP_REFER" value={heardAboutMapped} onChange={(e) => setHeardAboutMapped(e.target.value)} />
                     <label htmlFor="whyMappedUseful">Why would Mapped be useful to you?</label>
-                    <Input id="whyMappedUseful" value={whyMappedUseful} onChange={(e) => setWhyMappedUseful(e.target.value)} />
-                    <Button size='sm'>
+                    <Input id="whyMappedUseful" name="MAP_NEEDS" value={whyMappedUseful} onChange={(e) => setWhyMappedUseful(e.target.value)} />
+                    <Button
+                        variant="outline"
+                        onSubmit={() => {
+                            setMessage('Thank you for joining the waitlist');
+                        }}
+                    >
                         Sign up to the waitlist
                     </Button>
                 </form>
-            }
-            {message && <p>{message}</p>}
+            )}
         </>
     );
 };
-
 export default MailchimpForm;
