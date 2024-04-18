36480f84a9df36b9691e6d8b5155cc5d6aead262
Jan Baykara <janbaykara@pm.me>
2024-04-19 00:26:43 +0100

    Fix bug

--- hub/graphql/extensions/analytics.py
+++ hub/graphql/extensions/analytics.py
@@ -11,25 +11,22 @@ class APIAnalyticsExtension(SchemaExtension):
     def on_operation(self):
         request = self.execution_context.context.request
         if token_str := app_settings.JWT_TOKEN_FINDER(request):
-            try:
-                token = TokenType.from_token(token=token_str)
-            except Exception:
-                pass
-        signature = token.split(".")[2]
-        db_token = APIToken.objects.filter(signature=signature).first()
-        if db_token is not None:
-            print("Third party API call.")
-            user_or_error: UserOrError = get_user_or_error(request)
-            if user_or_error.error:
-                raise ValueError("User not found")
-            if db_token.revoked:
-                raise ValueError("Token has been revoked")
-            posthog.capture(
-                user_or_error.user.id,
-                "API request",
-                {
-                    "operation_name": self.execution_context.operation_name,
-                    "operation_type": self.execution_context.operation_type,
-                },
-            )
+            token = TokenType.from_token(token=token_str)
+            signature = token.token.split(".")[2]
+            db_token = APIToken.objects.filter(signature=signature).first()
+            if db_token is not None:
+                print("Third party API call.")
+                user_or_error: UserOrError = get_user_or_error(request)
+                if user_or_error.error:
+                    raise ValueError("User not found")
+                if db_token.revoked:
+                    raise ValueError("Token has been revoked")
+                posthog.capture(
+                    user_or_error.user.id,
+                    "API request",
+                    {
+                        "operation_name": self.execution_context.operation_name,
+                        "operation_type": self.execution_context.operation_type,
+                    },
+                )
         yield
