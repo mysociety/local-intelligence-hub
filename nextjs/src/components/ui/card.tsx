fad61713ed93a28996c00122ee4672bd7265bd54
Jan Baykara <janbaykara@pm.me>
2024-04-23 12:39:43 +0100

    Fix padding

--- nextjs/src/components/ui/card.tsx
+++ nextjs/src/components/ui/card.tsx
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col p-3", className)}
+    className={cn("flex flex-col", className)}
     {...props}
   />
 ))
@@ -60,7 +60,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-3", className)} {...props} />
+  <div ref={ref} className={cn("", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
