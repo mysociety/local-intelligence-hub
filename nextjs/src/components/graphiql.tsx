4787dbc6e2d96e97b0d2962155e1a7249301095a
Jan Baykara <janbaykara@pm.me>
2024-04-24 16:48:56 +0100

    Add more error catching

--- nextjs/src/components/graphiql.tsx
+++ nextjs/src/components/graphiql.tsx
@@ -5,25 +5,27 @@ import { GraphiQL, GraphiQLProps } from 'graphiql';
 import React, { useMemo } from 'react';
 import 'graphiql/graphiql.css';
 import { authenticationHeaders } from '@/lib/auth';
-
-function getAuthenticatedFetcher () {
-  try {
-    return createGraphiQLFetcher({
-      url: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/graphql`,
-      headers: authenticationHeaders(),
-      fetch: fetch
-    });
-  } catch (e) {
-    console.error(e)
-    return createGraphiQLFetcher({
-      url: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/graphql`,
-      fetch: fetch
-    });
-  }
-}
+import { ErrorBoundary } from '@sentry/nextjs';
 
 export function GraphQLPlayground(props: Partial<GraphiQLProps>) {
-  const fetcher = useMemo(getAuthenticatedFetcher, [])
+  const fetcher = useMemo(function getAuthenticatedFetcher () {
+    if (typeof window === 'undefined') return null
+    try {
+      return createGraphiQLFetcher({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/graphql`,
+        headers: authenticationHeaders()
+      });
+    } catch (e) {
+      console.error(e)
+      return createGraphiQLFetcher({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/graphql`
+      });
+    }
+  }, [])
   if (!fetcher) return null
-  return <GraphiQL {...props} fetcher={fetcher} />
+  return (
+    <ErrorBoundary>
+      <GraphiQL {...props} fetcher={fetcher} />
+    </ErrorBoundary>
+  )
 }
