9dd1ec2d788012ae5b31bc9bdd280b651e8c3f9f
Jan Baykara <janbaykara@pm.me>
2024-04-24 16:24:39 +0100

    Revert to old Graphiql page setup

--- nextjs/src/app/graphiql/page.tsx
+++ nextjs/src/app/graphiql/page.tsx
@@ -1,7 +1,26 @@
 "use client"
 
-import { GraphQLPlayground } from "@/components/graphiql"
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import { GraphiQL } from 'graphiql';
+import React, { useMemo } from 'react';
+import 'graphiql/graphiql.css';
+
+function getAuthenticatedFetcher () {
+  try {
+    const token = typeof window !== 'undefined' ? localStorage.getItem("jwt") : '';
+    return createGraphiQLFetcher({
+      url: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/graphql`,
+      headers: {
+        authorization: token ? `JWT ${token}` : "",
+      }
+    });
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 export default function Page () {
-  return <GraphQLPlayground />
+  const fetcher = useMemo(getAuthenticatedFetcher, [])
+  if (!fetcher) return null
+  return <GraphiQL fetcher={fetcher} />
 }
