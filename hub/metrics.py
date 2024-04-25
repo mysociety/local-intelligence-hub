5a7901b2b923b76c8084f1bf3d1bf916bd6efd87
Moggach <acunnane13@gmail.com>
2024-04-25 12:41:44 +0000

    set up two metrics to get number of rows updated in import and update data tasks

---
+++ hub/metrics.py
@@ -0,0 +1,26 @@
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+
+metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
+provider = MeterProvider(metric_readers=[metric_reader])
+
+# Sets the global default meter provider
+metrics.set_meter_provider(provider)
+
+# Creates a meter from the global meter provider
+jobs_meter = metrics.get_meter("meep.jobs")
+
+
+import_rows_requested = jobs_meter.create_histogram(
+    name="meep.jobs.import.rows_requested",
+    description="measures the number of rows to import"
+)
+
+update_rows_requested = jobs_meter.create_histogram(
+    name="meep.jobs.update.rows_requested",
+    description="measures the number of rows to update"
+)
