b91ae40e6ea88ba0b14d327cf6775d934f1c353e
Jan Baykara <janbaykara@pm.me>
2024-05-31 22:51:11 +0100

    Give SectionHeader a slug, so it can be linked to

--- nextjs/src/data/puck/config/blocks/SectionHeader/SectionHeader.tsx
+++ nextjs/src/data/puck/config/blocks/SectionHeader/SectionHeader.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { ComponentConfig } from "@measured/puck";
 import HubResponsivity from "../../template/HubReponsivity";
+import slugify from 'slug'
 
 export type SectionHeaderProps = {
     title: string;
+    slug: string;
     description: string;
-
 };
 
 export const SectionHeader: ComponentConfig<SectionHeaderProps> = {
@@ -14,20 +15,32 @@ export const SectionHeader: ComponentConfig<SectionHeaderProps> = {
         title: {
             type: "text",
         },
+        slug: {
+            type: "text"
+        },
         description: {
             type: "textarea",
         },
     },
+    resolveData: async ({ props }) => {
+      return {
+        props: {
+          ...props,
+          slug: slugify(props.title),
+        }
+      };
+    },
     defaultProps: {
         title: "Heading",
+        slug: "",
         description: "Dignissimos et eos fugiat. Facere aliquam corrupti est voluptatem veritatis amet id. Nam repudiandae accusamus illum voluptatibus similique consequuntur. Impedit ut rerum quae. Dolore qui mollitia occaecati soluta numquam. Non corrupti mollitia libero aut atque quibusdam tenetur."
     },
-    render: ({ title, description }) => {
+    render: ({ title, description, slug }) => {
         return (
             <HubResponsivity>
 
                 <div className="col-span-2 aspect-[2/1] rounded-[20px]  p-5 border border-jungle-green-200 flex flex-col gap-2 justify-end transition-all">
-                    <h2 className="lg:text-hub4xl text-hub3xl">{title}</h2>
+                    <h2 className="lg:text-hub4xl text-hub3xl" id={slug || slugify(title)}>{title}</h2>
                     <p className="text-xl">{description}</p>
                 </div>
             </HubResponsivity>
