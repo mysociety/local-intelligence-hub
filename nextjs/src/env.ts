c09b16d370893f80019549e880da5a5b869f1c37
Jan Baykara <jan@commonknowledge.coop>
2024-06-20 09:47:34 +0100

    Fix env

--- nextjs/src/env.ts
+++ nextjs/src/env.ts
@@ -1,6 +1,4 @@
-import env from 'env-var'
-
-export const BACKEND_URL = env.get('NEXT_PUBLIC_BACKEND_URL').default("http://127.0.0.1:8000").asString()
+export const BACKEND_URL =process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000"
 export const GRAPHQL_URL = new URL('/graphql', BACKEND_URL).toString()
-export const FRONTEND_URL = env.get('NEXT_PUBLIC_FRONTEND_URL').default("http://localhost:3000").asString()
+export const FRONTEND_URL =process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000"
 export const FRONTEND_HOSTNAME = new URL(FRONTEND_URL).hostname.split(':')[0]
