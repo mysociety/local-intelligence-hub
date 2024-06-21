a63bee767cf6536abe7218a7289a58f2d2089431
Jan Baykara <jan@commonknowledge.coop>
2024-06-21 22:30:26 +0100

    Add support for filtering on future/past events

--- hub/custom_lookups.py
+++ hub/custom_lookups.py
@@ -1,4 +1,5 @@
-from django.db.models import Transform, DateTimeField, BooleanField
+from django.db.models import BooleanField, DateTimeField, Transform
+
 
 @DateTimeField.register_lookup
 class IsPast(Transform):
@@ -7,7 +8,10 @@ class IsPast(Transform):
     def as_sql(self, compiler, connection):
         lhs, params = compiler.compile(self.lhs)
         field_name = self.lhs.output_field.name
-        return f"({lhs} IS NOT NULL AND {lhs} < CURRENT_TIMESTAMP) as {field_name}__{self.lookup_name}", params
+        return (
+            f"({lhs} IS NOT NULL AND {lhs} < CURRENT_TIMESTAMP) as {field_name}__{self.lookup_name}",
+            params,
+        )
 
     @property
     def output_field(self):
@@ -21,8 +25,37 @@ class IsFuture(Transform):
     def as_sql(self, compiler, connection):
         lhs, params = compiler.compile(self.lhs)
         field_name = self.lhs.output_field.name
-        return f"({lhs} IS NOT NULL AND {lhs} >= CURRENT_TIMESTAMP) as {field_name}__{self.lookup_name}", params
-    
+        return (
+            f"({lhs} IS NOT NULL AND {lhs} >= CURRENT_TIMESTAMP) as {field_name}__{self.lookup_name}",
+            params,
+        )
+
+    @property
+    def output_field(self):
+        return BooleanField()
+
+
+@DateTimeField.register_lookup
+class IsPastFilter(Transform):
+    lookup_name = "ispast_f"
+
+    def as_sql(self, compiler, connection):
+        lhs, params = compiler.compile(self.lhs)
+        return f"({lhs} IS NOT NULL AND {lhs} < CURRENT_TIMESTAMP)", params
+
+    @property
+    def output_field(self):
+        return BooleanField()
+
+
+@DateTimeField.register_lookup
+class IsFutureFillter(Transform):
+    lookup_name = "isfuture_f"
+
+    def as_sql(self, compiler, connection):
+        lhs, params = compiler.compile(self.lhs)
+        return f"({lhs} IS NOT NULL AND {lhs} >= CURRENT_TIMESTAMP)", params
+
     @property
     def output_field(self):
         return BooleanField()
