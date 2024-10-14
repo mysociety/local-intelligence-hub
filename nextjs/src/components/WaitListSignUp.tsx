09f506af71f163673e2fa7de62229133f5369bd7
Jan Baykara <janbaykara@pm.me>
2024-10-14 15:44:51 +0100

    Show waitlist submit button

--- nextjs/src/components/WaitListSignUp.tsx
+++ nextjs/src/components/WaitListSignUp.tsx
@@ -7,7 +7,7 @@ export default function WaitlistForm () {
       <IframeResizer
         src="https://us19.list-manage.com/survey?u=7d61a70102ab811e6282bee60&id=089628c6aa&attribution=false"
         className='rounded-md overflow-y-auto'
-        style={{ width: '1px', minWidth: '100%', minHeight: 1800 }}
+        style={{ width: '1px', minWidth: '100%', minHeight: 2600 }}
       />
     )
 };
