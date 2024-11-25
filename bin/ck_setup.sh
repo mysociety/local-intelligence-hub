3bb47766b411b5a58409d85246ebff7ece41a0bc
ev-sc <4164774+ev-sc@users.noreply.github.com>
2024-11-25 17:09:49 +0100

    Add bw sync command + friendly setup messages

--- bin/ck_setup.sh
+++ bin/ck_setup.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
-
-echo "Running Common Knowledge initial setup script"
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Setting up Mapped on your machine...';
+echo '';
+echo '------------------------------------';
 
 if [ ! -f ".env" ]; then
   echo -e "\e[31m.env file not found. Get this from BitWarden, then rebuild the dev container.\e[0m"
   exit 1
 fi
 
-echo -n "> Installing Python dependencies..."
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Setting up Python environment...';
+echo '';
+echo '------------------------------------';
 python -m venv .venv
 poetry install
 source .venv/bin/activate
 echo "Done"
 
-echo "> Setting up database..."
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Setting up database...';
+echo '';
+echo '------------------------------------';
 python manage.py migrate
 python manage.py createcachetable
 echo "Done"
-echo "> Populating database..."
+
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Seeding database...';
+echo '';
+echo '------------------------------------';
 python manage.py seed
-echo "> Importing LIH data..."
+
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Downloading LIH data...';
+echo '';
+echo '------------------------------------';
 python manage.py import_regions
 python manage.py import_areas
 python manage.py import_mps
+
+echo '';
+echo '------------------------------------';
+echo '';
 echo "> Downloading 2024 GE results CSV"
+echo '';
+echo '------------------------------------';
 curl 'https://researchbriefings.files.parliament.uk/documents/CBP-10009/HoC-GE2024-results-by-constituency.csv' \
   -H 'cache-control: no-cache' \
   -H 'referer: https://commonslibrary.parliament.uk/' \
@@ -32,7 +64,18 @@ curl 'https://researchbriefings.files.parliament.uk/documents/CBP-10009/HoC-GE20
   -o data/2024_general_election.csv
 python manage.py import_last_election_data
 
+echo '';
+echo '------------------------------------';
+echo '';
 echo -n "> Installing JavaScript dependencies..."
+echo '';
+echo '------------------------------------';
 cd nextjs
 npm i
-echo "Done"
+
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Setup complete!';
+echo '';
+echo '------------------------------------';
--- bin/lint.sh
+++ bin/lint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Linting Mapped...';
+echo '';
+echo '------------------------------------';
 black . && isort . && flake8
+
+echo '';
+echo '------------------------------------';
+echo '';
+echo 'Linting complete!';
+echo '';
+echo '------------------------------------';
