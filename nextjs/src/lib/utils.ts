115f919e6459924505cede043194ed9ca4527001
root <acunnane13@gmail.com>
2024-12-02 17:48:36 +0000

    add utility function to format CRM names before we render them

--- nextjs/src/lib/utils.ts
+++ nextjs/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function capitalizeWords(input: string): string {
+  return input
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+}
