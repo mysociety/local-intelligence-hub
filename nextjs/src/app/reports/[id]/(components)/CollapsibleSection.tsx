f22d01c2d20260acbf5a0f3eb7ad538334d1652d
ev <4164774+ev-sc@users.noreply.github.com>
2024-12-10 14:15:19 +0000

    Collapsible section

---
+++ nextjs/src/app/reports/[id]/(components)/CollapsibleSection.tsx
@@ -0,0 +1,62 @@
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible'
+import { TriangleDownIcon } from '@radix-ui/react-icons'
+import clsx from 'clsx'
+import { atom, useAtom } from 'jotai'
+import React, { useEffect } from 'react'
+
+interface Props {
+  id: string
+  title: string
+  children: React.ReactNode
+}
+
+const collapsibleSectionsAtom = atom<{ [key: string]: boolean }>({})
+
+const CollapsibleSection: React.FC<Props> = ({ children, id, title }) => {
+  const [sections, setSections] = useAtom(collapsibleSectionsAtom)
+  const thisSectionOpen = sections[id]
+
+  const toggleSection = (override: boolean) => {
+    setSections((prev) => ({
+      ...prev,
+      [id]: override !== undefined ? override : !prev[id],
+    }))
+  }
+
+  useEffect(() => {
+    toggleSection(true)
+  }, [])
+
+  return (
+    <Collapsible
+      key={id}
+      defaultOpen
+      className="mb-2"
+      onOpenChange={toggleSection}
+      open={!!thisSectionOpen}
+    >
+      <CollapsibleTrigger asChild>
+        <div className="flex flex-row gap-2 items-center my-3 cursor-pointer">
+          <TriangleDownIcon
+            className={clsx(
+              'h-4 w-4 text-white transition[-rotate-180] duration-700',
+              thisSectionOpen ? '' : 'rotate-180'
+            )}
+          />
+
+          <h3 className="text-white font-medium">{title}</h3>
+        </div>
+      </CollapsibleTrigger>
+
+      <CollapsibleContent className="CollapsibleContent">
+        {children}
+      </CollapsibleContent>
+    </Collapsible>
+  )
+}
+
+export default CollapsibleSection
--- nextjs/src/app/reports/[id]/(components)/ReportDataSources.tsx
+++ nextjs/src/app/reports/[id]/(components)/ReportDataSources.tsx
@@ -1,27 +1,13 @@
 import { AddMapLayerButton } from '@/components/report/AddMapLayerButton'
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@/components/ui/collapsible'
-import { ChevronsUpDown } from 'lucide-react'
 import useDataSources from '../useDataSources'
+import CollapsibleSection from './CollapsibleSection'
 import DataSourcesList from './DataSourcesList'
 
 export function ReportDataSources() {
   const { addDataSource } = useDataSources()
   return (
-    <Collapsible defaultOpen>
-      <div className="flex flex-row items-center gap-2">
-        <CollapsibleTrigger asChild>
-          <div className="flex flex-row gap-2 items-center my-3 cursor-pointer">
-            <ChevronsUpDown className="h-4 w-4 text-white" />
-            <h3 className="text-white font-medium">Data Sources</h3>
-          </div>
-        </CollapsibleTrigger>
-      </div>
-
-      <CollapsibleContent className="CollapsibleContent">
+    <div className="flex flex-col gap-2">
+      <CollapsibleSection id="report-data-sources" title="Data Sources">
         <p className="text-meepGray-300 text-sm mb-3 font-normal">
           Connect your own custom data sources or select data sources from our
           library.
@@ -30,7 +16,7 @@ export function ReportDataSources() {
         <div className="flex gap-2 items-center mt-1">
           <AddMapLayerButton addLayer={addDataSource} />
         </div>
-      </CollapsibleContent>
-    </Collapsible>
+      </CollapsibleSection>
+    </div>
   )
 }
