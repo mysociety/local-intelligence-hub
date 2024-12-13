bf42a4244669a166e52ed6c7ce50d77cd2ce8c53
Joaquim d'Souza <joaquimds@gmail.com>
2024-12-13 11:12:37 +0100

    feat: move unzip preview sql into import_preview_env_seed.sh

--- bin/import_preview_env_seed.sh
+++ bin/import_preview_env_seed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$ENVIRONMENT" = "staging" ]; then
+    unzip data/preview_env_seed.zip -d data
     cat data/preview_env_seed.psql | python manage.py dbshell
 else
     echo "This command runs only in the staging environment."
