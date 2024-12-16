5598f1b1a8b3beb9b302a20b13b9fc841f8a7b27
Jan Baykara <jan@commonknowledge.coop>
2024-12-16 13:11:30 +0000

    Fix migrations

--- hub/migrations/0146_batchrequest.py
+++ hub/migrations/0146_batchrequest.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("hub", "0144_hubimage_description"),
+        ("hub", "0145_localjsonsource"),
     ]
 
     operations = [
