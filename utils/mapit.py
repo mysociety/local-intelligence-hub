4c94bef55ad5f6b4fa28685498f18dcc368a2897
Jan Baykara <jan@commonknowledge.coop>
2024-12-19 14:58:55 +0000

    Fix other tests

--- utils/mapit.py
+++ utils/mapit.py
@@ -105,7 +105,7 @@ class MapIt(object):
                 gss_codes[area["type"]] = area["codes"]["gss"]
         return gss_codes
 
-    def areas_of_type(self, types, params):
+    def areas_of_type(self, types, params=None):
         url = self.areas_url % (self.base, ",".join(types), settings.MAPIT_API_KEY)
         data = self.get(url, params)
         areas = []
