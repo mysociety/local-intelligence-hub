dc3e5c782c4d554b7417fb22cd40b76a88bb3a42
Arbyhisenaj <41119392+Arbyhisenaj@users.noreply.github.com>
2025-01-09 13:21:05 +0000

    Correct opacity

--- nextjs/src/app/reports/[id]/(components)/MarkerPopup.tsx
+++ nextjs/src/app/reports/[id]/(components)/MarkerPopup.tsx
@@ -65,7 +65,7 @@ const MarkerPopup: React.FC<{ externalDataSourceId: string }> = ({
         anchor="bottom"
         offset={[0, -10] as any}
       >
-        <div className="relative z-10 rounded-lg w-48 p-2 bg-[#1f2229]/50  ">
+        <div className="relative z-10 rounded-lg w-48 p-2 bg-[#1f2229]/60  ">
           <div className=" backdrop-blur-[2px] rounded-lg p-4 absolute top-0 left-0 w-full h-full -z-10 border border-meepGray-600"></div>
           {selectedPointLoading ? (
             <div className="font-IBMPlexMono p-2 space-y-1 ">
