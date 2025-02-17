fb839cddb58bb68865280ddb5cddb073f963753a
Joaquim d'Souza <joaquimds@gmail.com>
2025-02-17 14:52:08 +0100

    fix: run populate_external_data_source_types in batches to avoid memory issues

--- hub/management/commands/populate_external_data_source_types.py
+++ hub/management/commands/populate_external_data_source_types.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from math import ceil
 
 from django.core.management.base import BaseCommand
 
@@ -7,6 +8,9 @@ from asgiref.sync import async_to_sync
 
 from hub.models import ExternalDataSource, GenericData
 from utils.statistics import merge_column_types, parse_and_type_json
+
+BATCH_SIZE = 10000
+DB_BATCH_SIZE = 100
 
 
 class Command(BaseCommand):
@@ -35,23 +39,30 @@ class Command(BaseCommand):
                 f"Processing source {i + 1} of {source_count}: {source} ({source.id})"
             )
             source: ExternalDataSource
-            data: list[GenericData] = GenericData.objects.filter(
+            qs: list[GenericData] = GenericData.objects.filter(
                 data_type__data_set__external_data_source_id=source.id
-            )
+            ).order_by("id")
             source_column_types = {}
 
-            print(f"--> Processing {len(data)} data points...")
+            data_count = qs.count()
 
-            for d in data:
-                parsed_json, column_types = parse_and_type_json(d.json)
-                merge_column_types(source_column_types, column_types)
-                d.parsed_json = parsed_json
+            print(f"--> Processing {data_count} data points...")
 
-            print("--> Saving processed data...")
+            batches = ceil(data_count / BATCH_SIZE)
+            offset = 0
+            for batch in range(0, batches):
+                data = qs[offset : offset + BATCH_SIZE]
+                for d in data:
+                    parsed_json, column_types = parse_and_type_json(d.json)
+                    merge_column_types(source_column_types, column_types)
+                    d.parsed_json = parsed_json
+                offset = offset + BATCH_SIZE
 
-            GenericData.objects.bulk_update(
-                data, fields=["parsed_json"], batch_size=100
-            )
+                print(f"--> Saving processed data batch {batch + 1} of {batches}...")
+
+                GenericData.objects.bulk_update(
+                    data, fields=["parsed_json"], batch_size=DB_BATCH_SIZE
+                )
 
             async_to_sync(source.update_field_definition_types)(source_column_types)
             source.last_import = datetime.now(tz=pytz.UTC)
