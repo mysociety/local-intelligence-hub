a192f1bb5742398e5a57b3fec260b871a98a568a
Struan Donald <struan@exo.org.uk>
2025-05-12 15:21:26 +0100

    initial import script and template for TCC act now campaign

---
+++ hub/management/commands/import_act_now_change_forever.py
@@ -0,0 +1,76 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+import pandas as pd
+from tqdm import tqdm
+
+from hub.models import Area, AreaAction, AreaActionData
+
+
+class Command(BaseCommand):
+    help = "Import Act Now Change Forever data"
+    data_file = settings.BASE_DIR / "data" / "act_now.csv"
+
+    def handle(self, quiet=False, *args, **options):
+        self._quiet = quiet
+        self.import_results()
+
+    def get_df(self):
+
+        if not self.data_file.exists():
+            self.stderr.write(f"Data file {self.data_file} does not exist")
+            return None
+
+        df = pd.read_csv(
+            self.data_file,
+            usecols=[
+                "gss_code",
+                "Constituency Time Slot",
+                "Venue",
+                "Address",
+                "Where to go",
+                "Room",
+            ],
+        )
+        df.columns = [
+            "gss_code",
+            "time_slot",
+            "venue",
+            "address",
+            "directions",
+            "room",
+        ]
+        df = df.fillna("")
+        return df
+
+    def import_results(self):
+
+        df = self.get_df()
+
+        if df is None or df.empty:
+            return
+
+        action, _ = AreaAction.objects.update_or_create(
+            name="2025_tcc_act_now",
+            defaults={
+                "label": "Act Now, Change Forever",
+                "is_public": True,
+                "visible": False,
+                "template": "_act_now.html",
+            },
+        )
+        for _, row in tqdm(df.iterrows()):
+            a = Area.objects.get(gss=row["gss_code"], area_type__code="WMC23")
+
+            AreaActionData.objects.update_or_create(
+                area=a,
+                action=action,
+                defaults={
+                    "data": {
+                        "time_slot": row["time_slot"],
+                        "venue": row["venue"],
+                        "address": row["address"],
+                        "directions": row["directions"],
+                    },
+                },
+            )
