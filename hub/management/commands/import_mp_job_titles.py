361a508065a3bffa6940122fd885bc3aac1a4400
Zarino Zappia <mail@zarino.co.uk>
2025-05-22 17:09:40 +0100

    Import MP positions from Parliament API rather than our own spreadsheet

--- hub/management/commands/import_mp_job_titles.py
+++ hub/management/commands/import_mp_job_titles.py
@@ -1,9 +1,8 @@
-import re
+from datetime import date
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
-import pandas as pd
+import requests
 from tqdm import tqdm
 
 from hub.models import AreaType, DataSet, DataType, Person, PersonData
@@ -11,37 +10,34 @@ from hub.models import AreaType, DataSet, DataType, Person, PersonData
 
 class Command(BaseCommand):
     help = "Import MP Job titles"
-    data_file = settings.BASE_DIR / "data" / "mp_positions_jul_2024.csv"
+
+    area_type = "WMC23"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-q", "--quiet", action="store_true", help="Silence progress bars."
+        )
 
     def handle(self, quiet=False, *args, **options):
         self._quiet = quiet
         self.import_results()
 
     def get_area_type(self):
-        return AreaType.objects.get(code="WMC23")
-
-    def get_df(self):
-
-        if not self.data_file.exists():
-            self.stderr.write(f"Data file {self.data_file} does not exist")
-            return None
-
-        df = pd.read_csv(
-            self.data_file,
-            usecols=["mp", "position"],
-        )
-        return df
+        return AreaType.objects.get(code=self.area_type)
 
     def create_data_type(self):
+        if not self._quiet:
+            self.stdout.write("Creating data set and type")
+
         mp_job_titles_ds, created = DataSet.objects.update_or_create(
             name="job_titles",
             defaults={
                 "data_type": "text",
                 "description": "Positions such as cabinet and shadow minister roles, spokespeople, and whips.",
-                "release_date": "July 2024",
+                "release_date": str(date.today()),
                 "label": "MP positions (job titles)",
-                "source_label": "Data compiled by mySociety from Gov.uk and Parliament.",
-                "source": "https://www.mysociety.org",
+                "source": "https://members-api.parliament.uk/",
+                "source_label": "Data from UK Parliament.",
                 "table": "people__persondata",
                 "comparators": DataSet.string_comparators(),
             },
@@ -57,28 +53,57 @@ class Command(BaseCommand):
 
         return mp_job_titles
 
-    def get_results(self, df: pd.DataFrame):
+    def get_results(self):
+        mps = PersonData.objects.filter(
+            data_type__name="parlid", person__person_type="MP"
+        ).select_related("person")
+
         results = {}
         if not self._quiet:
-            print("Matching MPs with titles")
-        for index, row in df.iterrows():
-            if pd.isna(row.mp):
+            self.stdout.write("Fetching MP positions")
+
+        for mp in tqdm(mps.all(), disable=self._quiet):
+            if mp.value() == "":  # pragma: no cover
+                print(f"problem with {mp.person.name} - no id")
                 continue
+
+            response = requests.get(
+                f"https://members-api.parliament.uk/api/Members/{mp.value()}/Biography"
+            )
+
             try:
-                person = Person.objects.get(name=row.mp, person_type="MP")
-                results[person] = row.position
-            except Person.DoesNotExist:
-                print(f"MP: {row.mp} not found.")
+                data = response.json()
+                posts = (
+                    data["value"]["governmentPosts"] + data["value"]["oppositionPosts"]
+                )
+                current_posts = [p for p in posts if p.get("endDate") is None]
+
+                for position in current_posts:
+                    # One dictionary key per MP, with multiple job titles
+                    # concatenated into a single linebreak-separated value.
+                    existing_position = results.get(mp.person.id, "")
+                    results[mp.person.id] = (
+                        f"{existing_position}\n{position['name']}".strip()
+                    )
+
+            except requests.RequestException:  # pragma: no cover
+                print(
+                    f"problem fetching info for {mp.person.name} with id {mp.value()}"
+                )
+
         return results
 
     def add_results(self, results, data_type):
+        if not self._quiet:
+            self.stdout.write("Updating MP positions")
+
         mp_list = []
-        for mp, job_title in tqdm(results.items(), disable=self._quiet):
-            job_title = re.sub("<br/?>", "\n", job_title)
+        for mp_id, job_title in tqdm(results.items(), disable=self._quiet):
+            person = Person.objects.get(id=mp_id)
             data, created = PersonData.objects.update_or_create(
-                person=mp,
+                person=person,
                 data_type=data_type,
-                data=job_title,
+                defaults={"data": job_title},
             )
             mp_list.append(data.id)
 
@@ -86,12 +111,6 @@ class Command(BaseCommand):
         PersonData.objects.filter(data_type=data_type).exclude(pk__in=mp_list).delete()
 
     def import_results(self):
-
-        df = self.get_df()
-
-        if df is None or df.empty:
-            return
-
         data_type = self.create_data_type()
-        results = self.get_results(df)
+        results = self.get_results()
         self.add_results(results, data_type)
