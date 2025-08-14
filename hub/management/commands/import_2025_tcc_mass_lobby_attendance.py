07bacab397079510b76fbc749a2767c728aaa7c8
Zarino Zappia <mail@zarino.co.uk>
2025-08-14 18:21:42 +0100

    Add importer for 2025 Mass Lobby attendance data

---
+++ hub/management/commands/import_2025_tcc_mass_lobby_attendance.py
@@ -0,0 +1,116 @@
+from django.conf import settings
+
+import pandas as pd
+
+from hub.models import AreaData, DataSet
+
+from .base_importers import BaseImportFromDataFrameCommand
+
+
+class Command(BaseImportFromDataFrameCommand):
+    help = "Import MP and constituent attendance of TCC’s 2025 Mass Lobby in London"
+
+    message = "importing TCC 2025 Mass Lobby attendance"
+    cons_row = "constituency_name"
+    cons_col = "constituency_name"
+    file_url = "https://docs.google.com/spreadsheets/d/1umLKyLyVBVCU023JSKvHO0nSbWZ-jiw6/export?format=csv"
+    uses_gss = False
+
+    area_type = "WMC23"
+
+    data_sets = {
+        "2025_mass_lobby_mp": {
+            "defaults": {
+                "label": "MP attendance at 2025 Act Now Change Forever Mass Lobby",
+                "data_type": "string",
+                "category": "movement",
+                "release_date": "July 2025",
+                "source_label": "Data collected by The Climate Coalition.",
+                "source": "https://www.theclimatecoalition.org/act-now-change-forever",
+                "source_type": "csv",
+                "data_url": "",
+                "table": "areadata",
+                "is_shadable": True,
+                "is_filterable": True,
+                "is_public": True,
+                "comparators": DataSet.in_comparators(),
+                "options": [
+                    {"title": "MP attended", "shader": "blue-500"},
+                    {"title": "MP sent a representative", "shader": "gray-500"},
+                    {"title": "MP did not attend", "shader": "gray-300"},
+                ],
+                "fill_blanks": False,
+            },
+            "col": "mp_enum",
+        },
+        "2025_mass_lobby_constituents": {
+            "defaults": {
+                "label": "Constituents attending 2025 Act Now Change Forever Mass Lobby",
+                "data_type": "integer",
+                "category": "movement",
+                "release_date": "July 2025",
+                "source_label": "Data collected by The Climate Coalition.",
+                "source": "https://www.theclimatecoalition.org/act-now-change-forever",
+                "source_type": "csv",
+                "data_url": "",
+                "table": "areadata",
+                "is_shadable": True,
+                "is_filterable": True,
+                "is_public": True,
+                "comparators": DataSet.numerical_comparators(),
+                "unit_type": "raw",
+                "unit_distribution": "people_in_area",
+                "fill_blanks": True,
+            },
+            "col": "total_constituents",
+        }
+    }
+
+    def get_dataframe(self):
+        df = pd.read_csv(
+            self.file_url,
+            usecols=[
+                "MP Name",
+                "Constituency",
+                "Did the MP attend?",
+                "Did MP send a representative instead?",
+                "Did MP or rep. attend?",
+                "Number of constituents registered (final numbers)"
+            ]
+        )
+
+        # Rename the columns, for simplicity
+        df.columns = [
+            "mp_name",
+            "constituency_name",
+            "mp_attended",
+            "aide_attended",
+            "mp_or_aide_attended",
+            "total_constituents"
+        ]
+
+        # Remove empty rows
+        df.dropna(subset=["constituency_name"], inplace=True)
+
+        # Fix broken constituency names in source spreadsheet
+        df["constituency_name"] = df["constituency_name"].replace({
+            "Montgomeryshire and GlyndÅµr": "Montgomeryshire and Glyndŵr",
+            "Taunton": "Taunton and Wellington",
+            "Ynys Man": "Ynys Môn",
+        })
+
+        # Replace empty values in the "total_constituents" column with 0
+        df["total_constituents"] = df["total_constituents"].fillna(0)
+
+        def summarise(row):
+            if row["mp_attended"] == "Yes":
+                return "MP attended"
+            elif row["aide_attended"] == "Yes":
+                return "MP sent a representative"
+            else:
+                return "MP did not attend"
+
+        # Calculate a tidied up column, that we will import from
+        df["mp_enum"] = df.apply(summarise, axis=1)
+
+        return df
