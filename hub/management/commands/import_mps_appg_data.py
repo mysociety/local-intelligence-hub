0775fea8ece1aa0ae0da52ffc983c8aced2f3bcc
Zarino Zappia <mail@zarino.co.uk>
2025-11-18 07:27:08 +0000

    Allow import of APPGs matching multiple categories
    Includes automatically switching between different categories for the
    different sites (`--site lih` vs `--site evaw` vs `--all_sites`).

--- hub/management/commands/import_mps_appg_data.py
+++ hub/management/commands/import_mps_appg_data.py
@@ -13,4 +13,31 @@ class Command(BaseMPAPPGMembershipImportCommand):
     register_source = source_base + "register.parquet"
     categories_source = source_base + "categories.parquet"
     members_source = source_base + "members.parquet"
-    category_slug = "ENVIRONMENT_CLIMATE_SUSTAINABILITY"
+
+    # Default category slugs (as a list to support multiple categories)
+    category_slugs = ["ENVIRONMENT_CLIMATE_SUSTAINABILITY"]
+
+    # Site-specific category slugs (each value is a list)
+    category_slugs_by_site = {
+        "lih": [
+            "ENVIRONMENT_CLIMATE_SUSTAINABILITY",
+            "INFRASTRUCTURE_TRANSPORT_MOBILITY",
+            "ENERGY_UTILITIES",
+        ],
+        "evaw": [
+            "HEALTH_MEDICINE_PUBLIC_HEALTH",
+            "HUMAN_RIGHTS_EQUALITY_SOCIAL_JUSTICE",
+            "SOCIAL_CARE_WELFARE_FAMILY_SUPPORT",
+        ]
+    }
+
+    def handle(self, *args, **options):
+        # Store the site argument to determine which category_slugs to use
+        site_arg = options.get("site", "")
+
+        # Set category_slugs based on the site
+        self.category_slugs = self.category_slugs_by_site.get(
+            site_arg, self.category_slugs)
+
+        # Call the parent handle method
+        super().handle(*args, **options)
