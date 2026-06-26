bf3ea5de526348f7a0c50162f711fc34cddd8d39
Zarino Zappia <mail@zarino.co.uk>
2026-06-26 17:36:00 +0100

    Updated (2023/24, released 2025) child poverty data
    You will need to download the XLSX file from https://endchildpoverty.org.uk/child-poverty-2025/
    and put it at `data/Child-poverty-AHC-2015-2024_final.xlsx` for the script to import.

--- hub/management/commands/import_child_poverty_data.py
+++ hub/management/commands/import_child_poverty_data.py
@@ -2,78 +2,100 @@ from django.db.models import FloatField
 
 import pandas as pd
 
-from hub.models import AreaData, DataSet
+from hub.import_utils import filter_authority_type
+from hub.models import DataSet
 
-from .base_importers import BaseImportFromDataFrameCommand
+from .base_importers import BaseImportFromDataFrameCommand, MultipleAreaTypesMixin
+
+DATA_FILE = "data/Child-poverty-AHC-2015-2024_final.xlsx"
+SOURCE_URL = "https://endchildpoverty.org.uk/child-poverty-2025/"
+
+YEARS = [
+    "2014/15",
+    "2015/16",
+    "2016/17",
+    "2017/18",
+    "2018/19",
+    "2019/20",
+    "2020/21",
+    "2021/22",
+    "2022/23",
+    "2023/24",
+]
+
+CONSTITUENCY_COLS = (
+    ["Region", "Constituency", "Area Code"]
+    + [f"Number {y}" for y in YEARS]
+    + [f"Percentage {y}" for y in YEARS]
+)
+
+LA_COLS = (
+    ["Region", "Local Authority", "Area Code"]
+    + [f"Number {y}" for y in YEARS]
+    + [f"Percentage {y}" for y in YEARS]
+)
+
+_DATASET_DEFAULTS = {
+    "label": "Estimated child poverty",
+    "description": "Percentage of children living in households with a net income (after housing costs) below 60% of the national median.",
+    "release_date": "June 2025",
+    "data_type": "percent",
+    "category": "place",
+    "source_label": "Data from End Child Poverty, based on data from DWP/HMRC.",
+    "source": SOURCE_URL,
+    "source_type": "xlsx",
+    "data_url": SOURCE_URL,
+    "table": "areadata",
+    "fill_blanks": False,
+    "comparators": DataSet.numerical_comparators(),
+    "default_value": 10,
+    "unit_type": "percentage",
+    "unit_distribution": "people_in_area",
+}
+
+CONSTITUENCY_DATA_SETS = {
+    "constituency_child_poverty": {
+        "defaults": _DATASET_DEFAULTS,
+        "col": "Percentage 2023/24",
+    }
+}
+
+LOCAL_AUTHORITY_DATA_SETS = {
+    "local_authority_child_poverty": {
+        "defaults": _DATASET_DEFAULTS,
+        "col": "Percentage 2023/24",
+    }
+}
 
 
-class Command(BaseImportFromDataFrameCommand):
+class Command(MultipleAreaTypesMixin, BaseImportFromDataFrameCommand):
     help = "Import data about child poverty"
 
-    source_url = "https://endchildpoverty.org.uk/child-poverty-2024/"
-    data_url = "https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/2010/Chid+poverty+AHC+2015-2023_Local+Authorities+and+Constituencies.xlsx?v=1716657555000"
-
+    source_url = SOURCE_URL
     cast_field = FloatField
     uses_gss = True
+    do_not_convert = True
     cons_row = "Area Code"
-    message = "Importing constituency child poverty data"
-    data_sets = {
-        "constituency_child_poverty": {
-            "defaults": {
-                "label": "Estimated child poverty",
-                "description": "Percentage of children living in households with a net income (after housing costs) below 60% of the national median.",
-                "release_date": "June 2024",
-                "data_type": "percent",
-                "category": "place",
-                "source_label": "Data from End Child Poverty, based on data from DWP/HMRC.",
-                "source": source_url,
-                "source_type": "xlxs",
-                "data_url": data_url,
-                "table": "areadata",
-                "fill_blanks": False,
-                "comparators": DataSet.numerical_comparators(),
-                "default_value": 10,
-                "unit_type": "percentage",
-                "unit_distribution": "people_in_area",
-            },
-            "col": "Percentage 2022-23",
-        }
-    }
+    message = "Importing child poverty data"
+    area_types = ["WMC23", "STC", "DIS"]
 
-    def delete_data(self):
-        AreaData.objects.filter(
-            data_type=self.data_types["constituency_child_poverty"]
-        ).delete()
+    @property
+    def data_sets(self):
+        if self.area_type == "WMC23":
+            return CONSTITUENCY_DATA_SETS
+        return LOCAL_AUTHORITY_DATA_SETS
 
     def get_dataframe(self):
-        df = pd.read_excel(
-            self.data_url, sheet_name="Parliamentary Constituency", skiprows=1
-        )
-        df.columns = [
-            "Region",
-            "Parliamentary Constituency",
-            "Area Code",
-            "Number 2014/15",
-            "Number 2015/16",
-            "Number 2016/17",
-            "Number 2017/18",
-            "Number 2018/19",
-            "Number 2019/20",
-            "Number 2020/21",
-            "Number 2021/22",
-            "Number 2022-23",
-            "Percentage 2014/15",
-            "Percentage 2015/16",
-            "Percentage 2016/17",
-            "Percentage 2017/18",
-            "Percentage 2018/19",
-            "Percentage 2019/20",
-            "Percentage 2020/21",
-            "Percentage 2021/22",
-            "Percentage 2022-23",
-            "Percentage point change (2015-22)",
-        ]
-        percentage_columns = [col for col in df.columns if "Percentage" in col]
-        for col in percentage_columns:
+        if self.area_type == "WMC23":
+            df = pd.read_excel(DATA_FILE, sheet_name="Constituency ", skiprows=1)
+            df.columns = CONSTITUENCY_COLS
+        else:
+            df = pd.read_excel(DATA_FILE, sheet_name="Local Authority", skiprows=1)
+            df.columns = LA_COLS
+            df = filter_authority_type(df, self.area_type, "Area Code")
+
+        percentage_cols = [col for col in df.columns if col.startswith("Percentage")]
+        for col in percentage_cols:
             df[col] = df[col] * 100
-        return df
+
+        return df.dropna(subset=["Percentage 2023/24"])
